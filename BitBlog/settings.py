af7bbb888afbfb75898604843c57a130817816a6
zahra <imivechi@gmail.com>
2022-04-21 22:31:51 +0430

    user app & posts app created

--- BitBlog/settings.py
+++ BitBlog/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # packages
+    'rest_framework',
+
+    # apps
+    'posts',
+    'accounts',
 ]
 
 MIDDLEWARE = [

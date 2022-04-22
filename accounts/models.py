631e2c1276dcee20ea72e44708812c31fba7484c
zahra <imivechi@gmail.com>
2022-04-22 19:56:17 +0430

    user manager created

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from accounts.managers import CustomUserManager
+
+
+class CustomUser(AbstractUser):
+    class Meta:
+        verbose_name = 'کاربر'
+        verbose_name_plural = 'کاربران'
+
+    username = None
+    email = models.EmailField(verbose_name='ایمیل', unique=True)
+    first_name = models.CharField(verbose_name='نام', max_length=150, blank=True)
+    last_name = models.CharField(verbose_name='نام خانوادگی', max_length=150, blank=True)
+    is_staff = models.BooleanField(
+        verbose_name='کارمند',
+        default=False,
+    )
+    is_active = models.BooleanField(
+        verbose_name='فعال',
+        default=True,
+
+    )
+    last_login = models.DateTimeField(verbose_name='آخرین ورود', blank=True, null=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
